48e368768fdc609d20c698d2912ffe6524a16b2d
CoderX <149763717+coderxsa@users.noreply.github.com>
2024-08-01 14:27:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
 global.email = "hacker@org";
 global.location = "Africa, Johannesbueg";
-global.mongodb = process.env.MONGODB_URI || "mongodb+srv://astrofx0011:astro@cluster0.lmwnxdt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://kadomew351:NTCEgsVqWUIrvF6I@cluster0.rsyakcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
